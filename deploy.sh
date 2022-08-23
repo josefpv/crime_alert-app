12dad5ed50ab5539d974b3f6672fd26f7b1c9083
josefpv <vivas.f13@gmail.com>
2022-08-22 22:54:46 -0400

    Deployment test.

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+docker build -t josefvivas/crime-alert-client:latest -t josefvivas/crime-alert-client:$SHA -f ./client/Dockerfile ./client
+docker build -t josefvivas/crime-alert-server:latest -t josefvivas/crime-alert-server:$SHA -f ./api/Dockerfile ./server
+
+docker push josefvivas/crime-alert-client:latest
+docker push josefvivas/crime-alert-server:latest
+
+docker push josefvivas/crime-alert-client:$SHA
+docker push josefvivas/crime-alert-server:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=josefvivas/crime-alert-server:$SHA
+kubectl set image deployments/client-deployment client=josefvivas/crime-alert-client:$SHA
